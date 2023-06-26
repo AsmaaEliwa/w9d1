a9f049e6a9c43ce3740d3935d92d6cede4c3ecbd
Manel-Os <oulds.manel@gmail.com>
2023-06-26 16:30:54 -0400

    Moving astroid

--- practice-for-ch-js-asteroids-long-practice-main/src/moving-object.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving-object.js
@@ -2,11 +2,13 @@ class MovingObject {
     constructor(canvas, optional = {}) {
         // debugger
         this.pos = optional.pos || [100, 100];
-        this.vel = optional.vel || [0, 0]
+        this.vel = optional.vel || [400, 400]
         this.radius = optional.radius || 10
         this.color = optional.color || "red"
         this.ctx = canvas.getContext("2d");
         this.draw(this.ctx);
+        this.move();
+        
     };
 
     draw(ctx) {
@@ -19,6 +21,14 @@ class MovingObject {
         ctx.fillStyle = this.color;
         ctx.fill();
         // ctx.stroke();
+
+    }
+
+    move(){
+        this.pos[0]+= this.vel[0];
+        this.pos[1]+= this.vel[1];
+        this.draw(canvas);
+        
 
     }
 
